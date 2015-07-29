2be95f8b9f2104c498762a008b0b467f67f3d54e
8l <plan9assembler@gmail.com>
2015-07-30 08:13:18 +0900

    initial go lang version 2009-11-06

---
+++ src/pkg/malloc/malloc.go
@@ -0,0 +1,24 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Go declarations for malloc.
+// The actual functions are written in C
+// and part of the runtime library.
+
+package malloc
+
+type Stats struct {
+	Alloc		uint64;
+	Sys		uint64;
+	Stacks		uint64;
+	InusePages	uint64;
+	NextGC		uint64;
+	EnableGC	bool;
+}
+
+func Alloc(uintptr) *byte
+func Free(*byte)
+func GetStats() *Stats
+func Lookup(*byte) (*byte, uintptr)
+func GC()
